9b5247524e8dd53e6ae125a90bb3caa3f79d1df8
ms5932867 <ms5932867@gmail.com>
2022-04-16 13:23:21 -0700

    Update Sort-MergeSort.java

--- Templates/Sort-MergeSort.java
+++ Templates/Sort-MergeSort.java
@@ -14,27 +14,20 @@ class Solution {
         merge(nums, s, m, e);
     }
     private void merge(int[] nums, int s, int m, int e) {
-        int[] B = new int[e - s + 1];
-        int index = 0;
+        int[] tmp = new int[e - s + 1];
         int i  = s;
         int j = m + 1;
-        while (i <= m && j <= e) {
-            if (nums[i] <= nums[j]) {
-                B[index] = nums[i];
+        for (int index = 0; index < tmp.length; index++) {
+            if (j > e ||  i <= m && nums[i] <= nums[j]) {
+                tmp[index] = nums[i];
                 i++;
             }
             else {
-                B[index] = nums[j];
+                tmp[index] = nums[j];
                 j++;
             }
-            index++;
+
         }
-        if (i <= m) {
-            System.arraycopy(nums, i, B, index, m - i + 1);
-        }
-        if (j <= e) {
-            System.arraycopy(nums,j, B, index, e - j + 1);
-        }
-        System.arraycopy(B, 0, nums, s, e - s + 1);
+        System.arraycopy(tmp, 0, nums, s, e - s + 1);
     }
 }
