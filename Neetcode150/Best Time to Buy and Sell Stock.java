cc05b2d07b154f522bebea9102fcfd0317e83559
ms5932867 <79813095+ms5932867@users.noreply.github.com>
2026-03-23 23:10:27 -0700

    2026-3-23

---
+++ Neetcode150/Best Time to Buy and Sell Stock.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = Integer.MIN_VALUE;
+        for (int p :prices) {
+            maxProfit = Math.max(maxProfit, p - min);
+            min = Math.min(min, p);
+        }
+        return maxProfit < 0 ? 0: maxProfit;
+    }
+}
+/**
+ * You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+
+You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+
+Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+Example 1:
+
+Input: prices = [10,1,5,6,7,1]
+
+Output: 6
+Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+Example 2:
+
+Input: prices = [10,8,7,5,2]
+
+Output: 0
+Explanation: No profitable transactions can be made, thus the max profit is 0.
+
+
+ */
